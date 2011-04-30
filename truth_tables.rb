37a7f12825f3cf393ccd65fd092e49621b72a072
Anita Kuno <ay.kuno@gmail.com>
2011-04-30 17:42:23 -0400

    yet more proofs

--- truth_tables.rb
+++ truth_tables.rb
@@ -67,3 +67,23 @@ puts %%The result of (false && false) && (true && false) is #{(false && false) &
 puts %%The result of (false && false) && (false && true) is #{(false && false) && (false && true)}%
 puts %%The result of (false && false) && (false && false) is #{(false && false) && (false && false)}%
 
+puts %% !(( A && B) || ( C && D )) ==  (( A || B) && ( C || D ))%
+puts %%The result of !(( true && true) || ( true && true )) == (( true || true) && ( true || true )) is #{!(( true && true) || ( true && true )) ==  (( true || true) && ( true || true ))}%
+puts %%The result of !(( true && true) || ( true && false )) == (( true || true) && ( true || false )) is #{!(( true && true) || ( true && false )) ==  (( true || true) && ( true || false ))}%
+puts %%The result of !(( true && true) || ( false && true )) == (( true || true) && ( false || true )) is #{!(( true && true) || ( false && true )) ==  (( true || true) && ( false || true ))}%
+puts %%The result of !(( true && true) || ( false && false )) == (( true || true) && ( false || false )) is #{!(( true && true) || ( false && false )) ==  (( true || true) && ( false || false ))}%
+
+puts %%The result of !(( true && false) || ( true && true )) == (( true || false) && ( true || true )) is #{!(( true && false) || ( true && true )) ==  (( true || false) && ( true || true ))}%
+puts %%The result of !(( true && false) || ( true && false )) == (( true || false) && ( true || false )) is #{!(( true && false) || ( true && false )) ==  (( true || false) && ( true || false ))}%
+puts %%The result of !(( true && false) || ( false && true )) == (( true || false) && ( false || true )) is #{!(( true && false) || ( false && true )) ==  (( true || false) && ( false || true ))}%
+puts %%The result of !(( true && false) || ( false && false )) == (( true || false) && ( false || false )) is #{!(( true && false) || ( false && false )) ==  (( true || false) && ( false || false ))}%
+
+puts %%The result of !(( false && true) || ( true && true )) == (( false || true) && ( true || true )) is #{!(( false && true) || ( true && true )) ==  (( false || true) && ( true || true ))}%
+puts %%The result of !(( false && true) || ( true && false )) == (( false || true) && ( true || false )) is #{!(( false && true) || ( true && false )) ==  (( false || true) && ( true || false ))}%
+puts %%The result of !(( false && true) || ( false && true )) == (( false || true) && ( false || true )) is #{!(( false && true) || ( false && true )) ==  (( false || true) && ( false || true ))}%
+puts %%The result of !(( false && true) || ( false && false )) == (( false || true) && ( false || false )) is #{!(( false && true) || ( false && false )) ==  (( false || true) && ( false || false ))}%
+
+puts %%The result of !(( false && false) || ( true && true )) == (( false || false) && ( true || true )) is #{!(( false && false) || ( true && true )) ==  (( false || false) && ( true || true ))}%
+puts %%The result of !(( false && false) || ( true && false )) == (( false || false) && ( true || false )) is #{!(( false && false) || ( true && false )) ==  (( false || false) && ( true || false ))}%
+puts %%The result of !(( false && false) || ( false && true )) == (( false || false) && ( false || true )) is #{!(( false && false) || ( false && true )) ==  (( false || false) && ( false || true ))}%
+puts %%The result of !(( false && false) || ( false && false )) == (( false || false) && ( false || false )) is #{!(( false && false) || ( false && false )) ==  (( false || false) && ( false || false ))}%
